e185574f5403dc5bbb7cc2137b092ea700b2058f
vlorg <vlorg@users.noreply.github.com>
2023-11-03 13:59:31 -0600

    Refactored to use buffering from persistent storage by saving the log in a flat file, and retrieving it into the chat buffer.

--- talkboxServer.js
+++ talkboxServer.js
@@ -1,10 +1,14 @@
+// noinspection JSValidateTypes
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const crypto = require('crypto');
+const fs = require('fs').promises;  // Use fs.promises for async operations
 
 const BUFFER_CAPACITY = 100;
 const SERVER_PORT = 3000;
+const BUFFER_FILE_PATH = './chatBuffer.json';
 
 const app = express();
 const server = http.createServer(app);
@@ -12,25 +16,56 @@ const io = socketIo(server, {path: '/talkbox/socket.io'});
 
 app.use('/talkbox', express.static(__dirname + '/public'));
 
+let isBufferLocked = false;
+
 class MessageBuffer {
     constructor(capacity) {
         this.buffer = Array(capacity).fill(null);
         this.index = 0;
     }
 
+    async init() {
+        try {
+            const data = await fs.readFile(BUFFER_FILE_PATH, 'utf8');
+            const savedBuffer = JSON.parse(data);
+            this.buffer = savedBuffer.buffer;
+            this.index = savedBuffer.index;
+        } catch (err) {
+            console.error('Error reading buffer file:', err);
+        }
+    }
+
     addMessage(msg) {
         this.buffer[this.index] = msg;
         this.index = (this.index + 1) % this.buffer.length;
+        this.syncToFile()
+            .catch(err => console.error('Error syncing to file:', err));
+    }
+
+    syncToFile() {
+        if (isBufferLocked) {
+            setTimeout(() => this.syncToFile(), 50);
+            return Promise.resolve();  // Return a resolved promise immediately
+        }
+
+        isBufferLocked = true;
+        return fs.writeFile(BUFFER_FILE_PATH, JSON.stringify({buffer: this.buffer, index: this.index}), 'utf8')
+            .finally(() => {
+                isBufferLocked = false;  // Ensure the lock is released
+            });
     }
 
     clear() {
         this.buffer.fill(null);
+        this.syncToFile();
     }
 
     getRecentMessages() {
         return this.buffer.filter(msg => msg !== null);
     }
 }
+
+let messageBuffer = new MessageBuffer(BUFFER_CAPACITY);
 
 class CommandHandler {
     static handleCommand(socket, msg, buffer) {
@@ -70,8 +105,6 @@ function createCommandMessage(timestamp, username, userId, message, command) {
     };
 }
 
-const messageBuffer = new MessageBuffer(BUFFER_CAPACITY);
-
 function md5(data) {
     return crypto.createHash('md5').update(data).digest('hex');
 }
@@ -106,6 +139,9 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(SERVER_PORT, () => {
-    console.log('listening on *:' + SERVER_PORT);
-});
+(async function initialize() {
+    await messageBuffer.init();
+    server.listen(SERVER_PORT, () => {
+        console.log('listening on *:' + SERVER_PORT);
+    });
+})();
