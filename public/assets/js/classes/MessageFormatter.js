c999c616313cb23386d43c0eaee71e907379c8b5
vlorg <vlorg@users.noreply.github.com>
2023-11-03 12:46:53 -0600

    Added two new methods for processing text.

--- public/assets/js/classes/MessageFormatter.js
+++ public/assets/js/classes/MessageFormatter.js
@@ -6,4 +6,21 @@ class MessageFormatter {
         const seconds = String(date.getSeconds()).padStart(2, '0');
         return `${hours}:${minutes}:${seconds}`;
     }
+
+    static convertLinksToAnchors(text) {
+        const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        return text.replace(urlRegex, (url) => {
+            return `<a href="${url}" target="_blank">${url}</a>`;
+        });
+    }
+
+    static convertTextToEmojis(text) {
+        const emojiRegex = /:([a-zA-Z0-9_]+):/g;
+        return text.replace(emojiRegex, (match, emojiName) => {
+            if (emojiName in emojiMap) {
+                return emojiMap[emojiName];
+            }
+            return match;
+        });
+    }
 }
